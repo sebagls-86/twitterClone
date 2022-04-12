914388094b9f9cb1d4907490681e72aa54d7f3a9
Seba <63512521+sebagls-86@users.noreply.github.com>
2022-04-12 20:17:58 -0300

    handler ok

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,10 +7,14 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"github.com/sebagls-86/twitterClone/mdw"
+	"github.com/sebagls-86/twitterClone/routers"
 )
 
 func Manejadores() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/registro", mdw.CheckBD(routers.Register)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {

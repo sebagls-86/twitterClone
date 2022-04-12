c816c846eb2b3cdf640857dbfb470842e9ba2e59
Seba <63512521+sebagls-86@users.noreply.github.com>
2022-04-12 14:29:40 -0300

    subida

---
+++ bd/conexionBD.go
@@ -0,0 +1,16 @@
+// package bd
+
+// import (
+// 	"context"
+// 	"log"
+
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// var MongoC = conectarBD()
+
+// mongodb := os.Getenv("MONGODB_URI")
+
+// var clientOptions = options.Client().ApplyURI(mongodb)
+
